1a34dd004f9c50f1704cc84963fad77c9280f3ec
hramtsov13 <hramtsov13@mail.ru>
2020-02-14 00:06:11 +0500

    Designed horiz images

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /*Show/hide my phone number*/
 
+
 function showPhoneNumber() {
   var x = document.getElementById("my-phone-number").style;
   if (x.visibility == "hidden" || x.visibility == "") {
     x.visibility = "visible";
   } else {
-    x.display = "hidden";
+    x.visibility = "hidden";
   }
 }
