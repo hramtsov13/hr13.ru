6c2aeb956b3865a8aadf20d1ad110726848858d0
hramtsov13 <hramtsov13@mail.ru>
2020-02-16 01:25:37 +0500

    OG image works and fonts scalable

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-/*Let's find WHAT we gonna show by clicking the button*/
+// Let's find WHAT we gonna show by clicking the button
 
 let myPhoneNumber = document.querySelector('.my-phone-number');
 
-/*Let's find the button*/
+// Let's find the button
 
 let showPhone = document.querySelector('.show-phone');
 
-/*Tell the button how to toggle*/
+// Tell the button how to toggle
 
 showPhone.onclick = function () {
   myPhoneNumber.classList.toggle('unclicked');
