0df81a20991bc69210a244e08c5bfc72dc2f7378
hramtsov13 <hramtsov13@mail.ru>
2020-02-17 10:43:29 +0500

    second section added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Let's find WHAT we gonna show by clicking the button
 
-let myPhoneNumber = document.querySelector('.my-phone-number');
+let togglePhone = document.querySelector('.my-phone-number');
 
 // Let's find the button
 
@@ -9,5 +9,35 @@ let showPhone = document.querySelector('.show-phone');
 // Tell the button how to toggle
 
 showPhone.onclick = function () {
-  myPhoneNumber.classList.toggle('unclicked');
+  togglePhone.classList.toggle('unclicked');
 }
+/*
+let toggleDescription = document.querySelector('.description-item1');
+let toggleDescription = document.querySelector('.description-item2');
+let toggleDescription = document.querySelector('.description-item3');
+let toggleDescription = document.querySelector('.description-item4');
+let menuButtonAbout = document.querySelector('.menu-list_root-about');
+let menuButtonPosition = document.querySelector('.menu-list_root-position');
+let menuButtonAchievments = document.querySelector('.menu-list_root-achievments');
+let menuButtonExample = document.querySelector('.menu-list_root-example');
+
+// 1st
+menuButtonAbout.onclick = function () {
+  toggleDescription.classList.toggle('description-item_about');
+}
+
+// 2nd
+menuButton.onclick = function () {
+  toggleDescription.classList.toggle('description-item_position');
+}
+
+// 3rd
+menuButton.onclick = function () {
+  toggleDescription.classList.toggle('description-item_achievments');
+}
+
+// 4th
+menuButton.onclick = function () {
+  toggleDescription.classList.toggle('description-item_example');
+}
+*/
