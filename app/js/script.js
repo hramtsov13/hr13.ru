a76f02d223976462064f17e5d1fa0ca4a4037d93
hramtsov13 <hramtsov13@mail.ru>
2020-08-19 20:38:51 +0500

    fixed slider crash by code doubling

--- app/js/script.js
+++ app/js/script.js
@@ -23,3 +23,31 @@ $(document).ready(function () {
     appendArrows: $(".slider__arrow"),
   });
 });
+
+document.addEventListener("swup:contentReplaced", (event) => {
+  $(document).ready(function () {
+    $(".facts__slider").slick({
+      arrows: true,
+      adaptiveHeight: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      touchThreshold: 10,
+      responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+      appendArrows: $(".slider__arrow"),
+    });
+  });
+});
+
+swup.on("contentReplaced", init);
